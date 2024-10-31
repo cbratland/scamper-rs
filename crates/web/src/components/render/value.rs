5e4ddcf9cb44e417cddb3b5ec368f6bd331e961c
cbratland <caelan.bratland@gmail.com>
2024-10-30 21:13:30 -0500

    fix: render values inside lists

--- crates/web/src/components/render/value.rs
+++ crates/web/src/components/render/value.rs
@@ -10,64 +10,132 @@ pub enum ValueOrError {
     Error(String),
 }
 
+fn custom_view(value: &Value) -> Option<View> {
+    match value {
+        Value::Struct(_) => {
+            fn render_rgb(rgb: Rgb, text: String) -> View {
+                let bg_color = rgb.to_string();
+                let text_color = rgb.pseudo_complement().to_string();
+                return view! {
+               		<div
+		        		style=format!(
+		          			"color: {}; background-color: {}; width: fit-content; border: 1px solid black; padding: 0.25em;",
+		             		text_color, bg_color
+		          		)
+		        	>
+						{text}
+					</div>
+			    }
+			    .into_view();
+            }
+            if let Some(rgb) = Rgb::from_value(&value) {
+                return Some(render_rgb(rgb, rgb.to_string()));
+            } else if let Some(hsv) = Hsv::from_value(&value) {
+                return Some(render_rgb(hsv.to_rgb(), hsv.to_string()));
+            }
+        }
+        Value::Foreign(item) => {
+            if let Some(drawing) = item.downcast_ref::<Drawing>() {
+                return Some(
+                    view! {
+                        <DrawingView drawing=drawing.clone() />
+                    }
+                    .into_view(),
+                );
+            } else if let Some(element) = item.downcast_ref::<LabElement>() {
+                return Some(
+                    view! {
+                        <LabElementView element=element.clone() />
+                    }
+                    .into_view(),
+                );
+            }
+        }
+        _ => {}
+    }
+    None
+}
+
 #[component]
 pub fn RenderedValue(value: ValueOrError) -> impl IntoView {
     match value {
-        ValueOrError::Error(error) => {
-            return view! {
-                <CodeBlock>
-                    {error}
-                </CodeBlock>
-            }
-            .into_view()
+        ValueOrError::Error(error) => view! {
+            <CodeBlock>
+                {error}
+            </CodeBlock>
         }
-        ValueOrError::Value(ref value) => match value {
-            Value::Struct(_) => {
-                fn render_rgb(rgb: Rgb, text: String) -> View {
-                    let bg_color = rgb.to_string();
-                    let text_color = rgb.pseudo_complement().to_string();
-                    return view! {
-	                	<div
-	                 		style=format!(
-	                   			"color: {}; background-color: {}; width: fit-content; border: 1px solid black; padding: 0.25em;",
-	                      		text_color, bg_color
-	                   		)
-	                 	>
-							{text}
-						</div>
-	                }
-	                .into_view();
-                }
-                if let Some(rgb) = Rgb::from_value(value) {
-                    return render_rgb(rgb, rgb.to_string());
-                } else if let Some(hsv) = Hsv::from_value(value) {
-                    return render_rgb(hsv.to_rgb(), hsv.to_string());
-                }
-            }
-            Value::Foreign(item) => {
-                if let Some(drawing) = item.downcast_ref::<Drawing>() {
-                    return view! {
-                        <DrawingView drawing=drawing.clone() />
-                    }
-                    .into_view();
-                } else if let Some(element) = item.downcast_ref::<LabElement>() {
-                    return view! {
-                        <LabElementView element=element.clone() />
-                    }
-                    .into_view();
-                }
-            }
-            _ => {}
-        },
-    }
+        .into_view(),
+        ValueOrError::Value(value) => {
+            if let Some(view) = custom_view(&value) {
+                view
+            } else {
+                match value {
+                    Value::List(values) => view! {
+                        <code class="hljs">
+                            <CodeBlock>
+                                "(list"
+                            </CodeBlock>
+                            <For
+                                each=move || values.clone()
+                                key=|value| format!("{:?}", value)
+                                children=move |value| {
+                                    let mut nodes = vec![view! {
+                                        <CodeBlock>" "</CodeBlock>
+                                    }.into_view()];
 
-    let ValueOrError::Value(value) = value else {
-        unreachable!()
-    };
-    view! {
-        <CodeBlock>
-            {value.to_string()}
-        </CodeBlock>
+                                    nodes.push(if let Some(view) = custom_view(&value) {
+                                        view
+                                    } else {
+                                        view! {
+                                            {value.to_string()}
+                                        }.into_view()
+                                    });
+
+                                    nodes
+                                }
+                            />
+                            <CodeBlock>
+                                ")"
+                            </CodeBlock>
+                        </code>
+                    }
+                    .into_view(),
+                    Value::Pair(px, py) => view! {
+                        <code class="hljs">
+                            <CodeBlock>
+                                "(pair "
+                            </CodeBlock>
+                            {if let Some(view) = custom_view(&px) {
+                                view
+                            } else {
+                                view! {
+                                    {px.to_string()}
+                                }.into_view()
+                            }}
+                            <CodeBlock>
+                                " "
+                            </CodeBlock>
+                            {if let Some(view) = custom_view(&py) {
+                                view
+                            } else {
+                                view! {
+                                    {py.to_string()}
+                                }.into_view()
+                            }}
+                            <CodeBlock>
+                                ")"
+                            </CodeBlock>
+                        </code>
+                    }
+                    .into_view(),
+                    _ => view! {
+                        <CodeBlock>
+                            {value.to_string()}
+                        </CodeBlock>
+                    }
+                    .into_view(),
+                }
+            }
+        }
     }
-    .into_view()
 }
